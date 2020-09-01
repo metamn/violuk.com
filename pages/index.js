e7f4cca004f9ed156659b919352141dfd04123fc
metamn <bartus.csongor@gmail.com>
2020-09-01 18:05:08 +0300

    Vimeo video added

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,25 @@
+import React from "react";
 import Head from "next/head";
+import ReactPlayer from "react-player";
+import styled from "styled-components";
 
-export default function Home() {
-  return "Hi!";
-}
+const Container = styled.section`
+  width: 100%;
+  height: 100%;
+  overflow: hidden;
+`;
+
+const Home = () => {
+  return (
+    <Container>
+      <ReactPlayer
+        url="https://vimeo.com/360846945"
+        playing={true}
+        width="100vw"
+        height="100vh"
+      />
+    </Container>
+  );
+};
+
+export default Home;
